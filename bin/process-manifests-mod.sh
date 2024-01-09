13fc35f806143d1b7d093c71ee3899199ecee75a
HuiQian Tan <huiqiantan@flywheel.io>
2024-01-09 15:22:01 -0500

    test git command

--- bin/process-manifests-mod.sh
+++ bin/process-manifests-mod.sh
@@ -323,24 +323,26 @@ function process_manifests() {
             fi
             env
             /qa-ci/scripts/run.sh job git_login
-            git add $V_MANIFEST_PATH
-            echo $GIT_COMMIT_CURRENT > $SENTINEL_FILENAME
-            cat $SENTINEL_FILENAME
-            git add $SENTINEL_FILENAME
-            echo "Process $manifest_type $manifest_name $manifest_version"
-            git commit -m "Process $manifest_type $manifest_name $manifest_version"
+            git branch
+            git status
+#            git add $V_MANIFEST_PATH
+#            echo $GIT_COMMIT_CURRENT > $SENTINEL_FILENAME
+#            cat $SENTINEL_FILENAME
+#            git add $SENTINEL_FILENAME
+#            echo "Process $manifest_type $manifest_name $manifest_version"
+#            git commit -m "Process $manifest_type $manifest_name $manifest_version"
 
             rm -rf $tempdir
         fi
     done
 
-    if git push -q $GIT_REMOTE $GIT_BRANCH; then
-        >&2 echo "Git push successful"
-    else
-        >&2 echo "Git push failed"
-        EXIT_STATUS=1
-        cleanup
-    fi
+#    if git push -q $GIT_REMOTE $GIT_BRANCH; then
+#        >&2 echo "Git push successful"
+#    else
+#        >&2 echo "Git push failed"
+#        EXIT_STATUS=1
+#        cleanup
+#    fi
 
 
 }
