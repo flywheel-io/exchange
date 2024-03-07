a61e977f50e59d8d2cef80be46f0bd9bad34a8e8
Pablo Velasco <pablovelasco@flywheel.io>
2024-03-07 18:05:09 -0500

    NEW: add_gear_categorization.py

---
+++ bin/add_gear_categorization.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+"""Add gear categorization to the different gears.
+
+This script adds the gear categorization from a CSV file to the different gears in the file.
+It adds it first to the manifest file and makes a commit to the gear repository, storing a list
+of files that have been modified. Then, it goes through the list of modified gears, takes the
+new manifest from the gear repository and modifies the corresponding manifest in the gear
+exchange repo in GitHub. It finally makes a commit to the gear exchange repository with all
+the changes
+"""
+
+import csv
+import json
+import logging
+import os
+import sys
+from argparse import ArgumentParser
+from glob import glob
+from shutil import rmtree
+from typing import List, Optional
+
+import utils
+
+GEAR_EXCHANGE_REPO = "https://gitlab.com/flywheel-io/scientific-solutions/gears/gear-exchange"
+NEW_BRANCH_NAME = "GEAR-5440-populate-gear-manifest-with-categorization"
+
+log = logging.getLogger(__name__)
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+
+def update_manifest_single_csv_row(
+        this_row: List[str],
+        header: List[str],
+        new_branch_name: str = NEW_BRANCH_NAME
+) -> Optional[utils.GearDetails]:
+    """Update the manifest for a single gear from a row in the CSV file.
+
+    Args:
+        this_row (List[str]): Row from the CSV file.
+        header (List[str]): Header of the CSV file.
+        new_branch_name (str): Name of the new branch to be created.
+
+    Returns:
+        Optional[utils.GearDetails]: Gear details if the gear was processed, None otherwise.
+    """
+    gear_name = this_row[header.index("name")]
+    # get the manifest file and the URL of the repo for the gear:
+    row_manifest_url, repo_url = utils.get_manifest_url_and_repo_url(this_row, header)
+    if not row_manifest_url:
+        log.warning("Could not find manifest file for gear: %s", gear_name)
+        return None
+    log.info("Processing gear: %s", gear_name)
+
+    this_gear_categories = utils.extract_gear_categories(this_row, header)
+
+    gear_repo = utils.clone_repo(repo_url)
+    log.debug("local repo path: %s", gear_repo.working_tree_dir)
+
+    utils.update_manifest(gear_repo, this_gear_categories, repo_url)
+
+    changes_to_repo = utils.commit_and_push_changes_to_manifest(
+        gear_repo,
+        new_branch_name=new_branch_name,
+        commit_message="UP: add gear categories to manifest.json"
+    )
+
+    if changes_to_repo:
+        utils.merge_project(
+            repo_url,
+            source_branch=new_branch_name,
+            target_branch="main" if "gitlab" in repo_url.removeprefix("https://") else "master",
+            title=new_branch_name
+        )
+
+    # recursively delete the temp file with the local repo:
+    rmtree(os.path.dirname(gear_repo.working_tree_dir))
+
+    return utils.GearDetails(gear_name, repo_url)
+
+
+def update_manifest_in_gear_repos(
+        csv_file_path: os.PathLike,
+        new_branch_name: str = NEW_BRANCH_NAME,
+) -> List[utils.GearDetails]:
+    """Update the manifest file in the gear repositories with info from csv file.
+
+    Args:
+        csv_file_path (os.PathLike): Path to the CSV file with the gear categorization.
+        new_branch_name (str): Name of the new branch to be created.
+
+    Returns:
+        list[utils.GearDetails]: List of gears that have been processed.
+    """
+
+    with open(csv_file_path, newline='', mode='r', encoding='utf-8-sig') as csvfile:
+        csvreader = csv.reader(csvfile)
+        # first row is the header line:
+        header = next(csvreader)
+        # check if the header contains all the categories:
+        if any([category not in header for category in utils.CATEGORIES_DICT]):
+            log.warning(f"Header does not contain all categories: {utils.CATEGORIES_DICT.keys()}")
+
+        processed_gears = []
+        # iterate over the gears in the file:
+        for row in csvreader:
+            # if gear is tagged to be deprecated, skip it:
+            if row[header.index("recom. action")] == "DEP":
+                log.debug("Skipping deprecated gear: %s", row[header.index("name")])
+                continue
+
+            # if everything went well, add the gear to the list of processed gears:
+            # (we store the repo_url so that we don't need to search for it later)
+            this_gear = update_manifest_single_csv_row(row, header, new_branch_name)
+            if this_gear:
+                log.info("Done processing gear: %s", this_gear.gear_name)
+                processed_gears.append(this_gear)
+            else:
+                log.warning("Could not process gear: %s", row[header.index("name")])
+
+    return processed_gears
+
+
+def find_matching_manifest_files_in_repo(
+        gear_to_match: utils.GearDetails,
+        exchange_repo: utils.Repo
+) -> Optional[str]:
+    """Find manifests with the same gear_name in local repo.
+
+    Manifests can be in different folders if there are custom versions of the gear besides
+    the official flywheel one. If so, we'll try to find the one in the folder that matches
+    the root of the gear image (Docker image).
+
+    Args:
+        gear_to_match (utils.GearDetails): Gear to match.
+        exchange_repo (utils.Repo): Repository with the manifests.
+
+    Returns:
+        Optional[str]: Absolute path to the matching manifest file.
+    """
+    matching_manifests = glob(
+        os.path.join(
+            exchange_repo.working_tree_dir, "gears", "*", f"{gear_to_match.gear_name}.json"
+        )
+    )
+    if not matching_manifests:
+        log.warning("Could not find manifest file for gear: %s", gear_to_match.gear_name)
+        return None
+
+    if len(matching_manifests) == 1:
+        return matching_manifests[0]
+
+    # More than one manifest with the same name found, in different folders.
+
+    # Check first to see if we can find one in the folder with name of the gear image root.
+    # Let's get the actual gear manifest from the (remote) gear repo:
+    manifest_content = utils.get_manifest_content_from_remote(gear_to_match.repo_manifest_url)
+    docker_image = manifest_content.get("custom", {}).get("gear-builder", {}).get("image", "")
+    docker_image_root = docker_image.split("/")[0]
+
+    # We'll try in the folder with the same root as the gear image (Docker image):
+    for manifest in matching_manifests:
+        if os.path.basename(os.path.dirname(manifest)) == docker_image_root:
+            return manifest
+
+    # We didn't find one. Give a warning
+    log.warning("Could not find a matching manifest for gear: %s", gear_to_match.gear_name)
+    log.warning("Matching manifests: %s", matching_manifests)
+
+
+def update_exchange_manifests(
+        gear_list: List[utils.GearDetails],
+        new_branch_name: str = NEW_BRANCH_NAME,
+) -> None:
+    """Update the different manifests in the gear exchange repository.
+
+    Args.
+        gear_list (List[utils.GearDetails]): List of gears details that have been processed.
+        new_branch_name (str): Name of the new branch to be created in the gear exchange repo.
+    """
+    # clone the gear exchange repo
+    exchange_repo = utils.clone_repo(GEAR_EXCHANGE_REPO)
+
+    # iterate over the processed gear list: grab the manifest from the gear repo and update
+    # the corresponding manifest in the gear exchange:
+    for gear in gear_list:
+        if not gear.repo_manifest_url:
+            log.warning("Could not find manifest file for gear: %s", gear.gear_name)
+            continue
+
+        if not (target_manifest := find_matching_manifest_files_in_repo(gear, exchange_repo)):
+            # Skip this gear. The function find_matching_manifest_files_in_repo will log a warning
+            continue
+
+        # we have a target_manifest. Update it with the content of the manifest from the gear repo:
+        with open(target_manifest, 'w') as file:
+            manifest_content = utils.get_manifest_content_from_remote(gear.repo_manifest_url)
+            json.dump(manifest_content, file, indent=2)
+
+    # Now, commit the changes (in a single commit) to the gear exchange repo and push them to the remote
+    exchange_repo_changed = utils.commit_manifest_changes_to_gear_exchange_repo(
+        exchange_repo,
+        new_branch_name=NEW_BRANCH_NAME,
+        commit_message="UP: update manifests with gear categorization",
+    )
+
+    if exchange_repo_changed:
+        # create a pull request in the gear exchange repo
+        utils.merge_project(
+            GEAR_EXCHANGE_REPO,
+            source_branch=NEW_BRANCH_NAME,
+            target_branch="main" if "gitlab" in GEAR_EXCHANGE_REPO.removeprefix("https://") else "master",
+            title=NEW_BRANCH_NAME
+        )
+
+    # recursively delete the temp file with the local repo:
+    rmtree(os.path.dirname(exchange_repo.working_tree_dir))
+
+
+def main(csv_file_path: os.PathLike, new_branch_name: str = NEW_BRANCH_NAME):
+    """Main function of the script."""
+    logging.basicConfig(level=logging.INFO)
+    log.info("Adding gear categorization to gears from CSV file: %s", csv_file_path)
+    updated_gears = update_manifest_in_gear_repos(csv_file_path, new_branch_name)
+    # if you want to save the list of updated gears to a file, you can do it here:
+    utils.save_updated_files_to_csv(updated_gears, "updated_gears.csv")
+    update_exchange_manifests(updated_gears)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Add gear categorization from CSV file")
+    parser.add_argument("--csv", help="CSV file with gear categorization", required=True)
+    parser.add_argument("--new_branch_name", help="Name of the new branch to be created", default=NEW_BRANCH_NAME)
+
+    args = parser.parse_args()
+
+    main(args.csv, args.new_branch_name)
---
+++ bin/utils.py
@@ -0,0 +1,438 @@
+import json
+import logging
+import os
+import requests
+import tempfile
+from collections.abc import Mapping
+from copy import deepcopy
+from time import sleep
+from typing import List, Optional, Tuple
+from urllib.parse import urlparse
+
+from dotty_dict import dotty
+from git import Repo
+
+# dictionary to map the categories in the CSV file to the categories in the Flywheel manifest:
+CATEGORIES_DICT = {
+    "category": "custom.gear-builder.category",
+    "Species": "custom.flywheel.classification.species",
+    "Organ": "custom.flywheel.classification.organ",
+    "Therapeutic Area": "custom.flywheel.classification.therapeutic_area",
+    "Modality": "custom.flywheel.classification.modality",
+    "Function": "custom.flywheel.classification.function",
+    "Suite": "custom.flywheel.suite",
+    "Gear Permission": "gear_permission",  # <- this one is special, we'll remove before
+    #                                           updating manifest
+}
+# those categories that can have multiple values:
+LIST_TYPE_CATEGORIES = [
+    "Species",
+    "Organ",
+    "Therapeutic Area",
+    "Modality",
+    "Function",
+]
+# Verify that all categories in LIST_TYPE_CATEGORIES are in CATEGORIES_DICT:
+if any([category not in CATEGORIES_DICT for category in LIST_TYPE_CATEGORIES]):
+    raise ValueError(
+        f"LIST_TYPE_CATEGORIES contains categories not in CATEGORIES_DICT: {LIST_TYPE_CATEGORIES}"
+    )
+
+
+# get the private tokens from the environment:
+GITLAB_PRIVATE_TOKEN = os.environ.get('GITLAB_PRIVATE_TOKEN')
+if not GITLAB_PRIVATE_TOKEN:
+    raise RuntimeError("GITLAB_PRIVATE_TOKEN environment variable not set.")
+GITHUB_PRIVATE_TOKEN = os.environ.get('GITHUB_PRIVATE_TOKEN')
+if not GITHUB_PRIVATE_TOKEN:
+    raise RuntimeError("GITHUB_PRIVATE_TOKEN environment variable not set.")
+
+
+log = logging.getLogger(__name__)
+
+
+class GearDetails:
+    """Class to hold the details of a gear."""
+    def __init__(self, name, url):
+        self.gear_name = name
+        self.repo_manifest_url = url
+
+    def __str__(self):
+        return f"{self.gear_name}, {self.repo_manifest_url}"
+
+    def __repr__(self):
+        return f"{self.gear_name}, {self.repo_manifest_url}"
+
+
+def gitlab_or_github_repo(repo_url: str) -> Optional[str]:
+    """Return "gitlab" or "github" if the repo is in GitLab or GitHub or none if it's neither.
+
+    Check the host (we don't just assume that if "gitlab" in string, it is Gitlab, because
+    it could be part of the repo name):
+    """
+    domain = urlparse(repo_url).netloc
+    if "gitlab" in domain:
+        return "gitlab"
+    if "github" in domain:
+        return "github"
+    return None
+
+
+def get_manifest_content_from_remote(manifest_url: str) -> Optional[dict]:
+    """Retrieve the actual manifest file from the URL of the remote repository.
+
+    Args:
+        manifest_url (str): URL of the manifest file.
+
+    Returns:
+        Optional[dict]: The content of the manifest file.
+    """
+    try:
+        response = requests.get(manifest_url)
+    except requests.exceptions.RequestException as e:
+        log.warning("Error getting manifest.json from %s: %s", manifest_url, e)
+        return None
+
+    if response.status_code != 200:
+        log.warning("Could not download the manifest.json at %s", manifest_url)
+        return None
+
+    return response.json()
+
+
+def get_repo_manifest_url(repo_url: str) -> Optional[str]:
+    """Get the manifest url.
+
+    Try to see if there is a valid manifest.json file in the repo. Try "main" and "master"
+    branches.
+
+    Args:
+        repo_url (str): URL of the repository.
+
+    Returns:
+        Optional[str]: URL of the manifest file.
+    """
+    server_type = gitlab_or_github_repo(repo_url)
+    if server_type == "gitlab":
+        raw_content_url_base = f"{repo_url}/-/raw"
+    elif server_type == "github":
+        raw_content_url_base = (
+            "https://raw.githubusercontent.com/"
+            f"{repo_url.split('github.com/')[1]}"
+        )
+    else:
+        log.warning("Could not determine server type for %s", repo_url)
+        return None
+
+    for branch in ["main", "master"]:
+        manifest_url = f"{raw_content_url_base}/{branch}/manifest.json"
+        manifest_dict = get_manifest_content_from_remote(manifest_url)
+        if manifest_dict and (
+            manifest_dict.get("custom", {}).get("gear-builder") or
+            manifest_dict.get("custom", {}).get("docker-image")
+        ):
+            log.debug("Found manifest.json in %s", manifest_url)
+            return manifest_url
+
+    log.warning("Could not find a gear manifest.json in %s main or master branches", repo_url)
+    return None
+
+
+def get_manifest_url_and_repo_url(
+        this_row: List[str], header: List[str],
+) -> Tuple[Optional[str], Optional[str]]:
+    """Get the manifest file and the URL of the repo for the gear.
+
+    There are two URLs, and sometimes they have been used interchangeably. So find the one that
+    contains a "manifest.json" file in it, with preference for repos in GitLab.
+
+    Args:
+        this_row (List[str]): the row in the CSV file.
+        header (List[str]): the header of the CSV file.
+
+    Returns:
+        Optional[str]: the URL of the manifest file if found, None otherwise.
+        Optional[str]: the URL of the repo if found, None otherwise.
+    """
+    url_column_names = ["source", "url"]
+    repos = [this_row[header.index(url)] for url in url_column_names]
+    # we give preference to Gitlab repos, so we'll check them first:
+    gitlab_repo = next((repo for repo in repos if "gitlab" in repo), None)
+    if gitlab_repo:
+        manifest_url = get_repo_manifest_url(gitlab_repo)
+        if manifest_url:
+            return manifest_url, gitlab_repo
+        # if we didn't find it, remove this repo from the list of repos to process:
+        repos.remove(gitlab_repo)
+    # if we're here, we haven't found the manifest yet. Try the rest of the repos:
+    for repo in repos:
+        manifest_url = get_repo_manifest_url(repo)
+        if manifest_url:
+            return manifest_url, repo
+    # if we're here, we haven't found the manifest in any of the repos:
+    return None, None
+
+
+def extract_gear_categories(row: list, header: list) -> dict:
+    """Extract the gear categories from the row.
+
+    Use a dotty dictionary to store the categories, so we can set them using dot notation.
+    Then, return a regular (nested) dictionary.
+    """
+    gear_categories = dotty()
+    for c, field in CATEGORIES_DICT.items():
+        # if this category is one of those that can have multiple values:
+        if c in LIST_TYPE_CATEGORIES:
+            # split the values by ", " and store them as a list.
+            # Then, to make sure we don't have repeated values, convert the list to a set and back to a list:
+            gear_categories[field] = list(set(row[header.index(c)].split(", ")))
+        else:
+            gear_categories[field] = row[header.index(c)]
+
+    return gear_categories.to_dict()
+
+
+# def fix_gear_url_and_source(url: str) -> str:
+
+
+def set_gear_permission(permission: str, current_manifest: dict):
+    """Set the gear permission in the manifest."""
+    # in case the permission is an empty string:
+    if not permission:
+        permission = "None"
+    # First, check that the permission is valid:
+    if permission not in ["None", "Read Only", "Read-write", "Admin"]:
+        raise ValueError(f"Invalid gear permission: {permission}")
+    # Now, if the permission is "None":
+    if permission == "None":
+        # check that the "inputs.api-key" is not in the current manifest:
+        if current_manifest["inputs"].get("api-key", None):
+            raise ValueError(
+                "Gear permission should be 'None', but 'inputs.api-key' present in manifest."
+            )
+        return
+    if permission == "Admin":
+        raise ValueError("'Admin' gears are not supported at this time.")
+    # set "inputs.api-key.read-only" to True if permission is "Read Only", False otherwise:
+    current_manifest["inputs"]["api-key"]["read-only"] = permission == "Read Only"
+
+
+def clone_repo(repo_url) -> Repo:
+    """Clone the repo in the given URL.
+
+    We'll do a shallow clone, so we don't need to clone the entire history of the repo.
+    We'll use the SSH URL, so we don't need to provide credentials to push the changes.
+
+    Returns:
+        Repo: a gitpython Repo object with the cloned repo.
+    """
+    repo_name = repo_url.split('/')[-1].replace('.git', '')
+    # create a temp dir in which to clone the repo:
+    cloned_repo_path = os.path.join(tempfile.mkdtemp(prefix="gear_categorization_"), repo_name)
+    # check to see if the url is https or ssh (we want ssh):
+    if repo_url.startswith("https://"):
+        # "https://github.com/foo/bar" -> "git@github.com:foo/bar.git"
+        # "https://gitlab.com/foo/bar" -> "git@gitlab.com:foo/bar.git"
+        # replace "https://" with "git@", then the first "/" with ":" and attach ".git" at the end:
+        repo_url = repo_url.replace("https://", "git@", 1).replace("/", ":", 1)
+        if not repo_url.endswith(".git"):
+            repo_url += ".git"
+    return Repo.clone_from(repo_url, cloned_repo_path, depth=1)
+
+
+def commit_and_push_changes_to_manifest(
+        repo: Repo, new_branch_name="Update_manifest", commit_message="Update manifest.json"
+) -> bool:
+    """Commit and push the changes to the manifest a new branch.
+
+    Args:
+        repo (Repo): the gitpython Repo object for the repository.
+        new_branch_name (str): name of the new branch to be created in the repo.
+        commit_message (str): commit message for the changes.
+
+    Returns:
+        bool: True if there were changes to commit and push, False otherwise.
+    """
+    new_branch = repo.create_head(new_branch_name)
+    new_branch.checkout()
+    repo.git.pull()   # in case there are changes in the remote repo
+    # make sure we have only changed the manifest:
+    repo_changes = [item.a_path for item in repo.index.diff(None)]
+    if "manifest.json" not in repo_changes:
+        log.warning("No changes to the manifest.json")
+        return False
+
+    if repo_changes == ["manifest.json"]:
+        repo.index.add(['manifest.json'])
+        repo.index.commit(commit_message)
+        repo.remotes.origin.push(refspec=f"{new_branch.name}:{new_branch.name}")
+        return True
+
+    log.warning("There were changes to files other than manifest.json")
+    log.warning("Changes: %s", repo_changes)
+    log.warning("Not committing or pushing changes.")
+    return False
+
+
+def deep_merge(dict1, dict2):
+    """ Return a new dictionary by merging two dictionaries recursively. """
+    result = deepcopy(dict1)
+    for key, value in dict2.items():
+        if isinstance(value, Mapping):
+            result[key] = deep_merge(result.get(key, {}), value)
+        else:
+            result[key] = deepcopy(dict2[key])
+    return result
+
+
+def update_manifest(repo: Repo, update_info: dict, repo_url: str):
+    """Update the Repo manifest file with the update_info, and check gear_url."""
+    manifest_path = os.path.join(repo.working_tree_dir, 'manifest.json')
+    if os.path.exists(manifest_path):
+        with open(manifest_path, 'r+') as file:
+            manifest = json.load(file)
+            # set the gear permission (and remove it from the update_info):
+            gear_permission = update_info.pop("gear_permission", "None")
+            set_gear_permission(gear_permission, manifest)
+            # set all the other properties:
+            try:
+                manifest = deep_merge(manifest, update_info)
+            except Exception as e:
+                log.error(f"Error updating manifest: {manifest_path}")
+                raise e
+            # check that the gear_url is the same as the repo_url:
+            if not manifest.get("url") == repo_url:
+                log.warning("Gear URL in manifest.json does not match the repo URL.")
+                log.warning("Switching gear URL and source.")
+                new_source = manifest.get("source", "")
+                manifest["url"] = repo_url
+                manifest["source"] = new_source
+
+            # overwrite the manifest file with the updated manifest:
+            file.seek(0)
+            json.dump(manifest, file, indent=2)
+            file.write('\n')
+            file.truncate()
+    else:
+        log.warning(f"Manifest.json not found in {repo.working_tree_dir}")
+
+
+gitlab_repos = {}
+github_repos = {}
+
+
+def merge_project(
+        remote_repo: str,
+        source_branch: str,
+        target_branch: str,
+        title: str = "Update manifest.json"
+):
+    """Create a merge request in the given project.
+
+    Arguments:
+        remote_repo (str): URL of the remote repository.
+        source_branch (str): name of the source branch.
+        target_branch (str): name of the target branch.
+        title (str): title of the merge request.
+    """
+    description = "Update manifest.json with gear categories"
+
+    def merge_project_gitlab():
+        """Create a merge request in a GitLab project."""
+        if not gitlab_repos:
+            from gitlab import Gitlab
+        # get the Gitlab object for the given URL:
+        if host_url not in gitlab_repos:
+            gitlab_repos[host_url] = Gitlab(host_url, private_token=GITLAB_PRIVATE_TOKEN)
+        gl = gitlab_repos[host_url]
+
+        project = gl.projects.get(repo_path)
+        mr = project.mergerequests.create({
+            "source_branch": source_branch,
+            "target_branch": target_branch,
+            "title": title,
+            "description": description,
+            "remove_source_branch": True,
+            "allow_maintainer_to_push": True,
+            "approvals_before_merge": 0,
+        })
+        # it looks like if you try to merge a MR that is not ready, it will fail.
+        sleep(15)
+        mr.merge(merge_when_pipeline_succeeds=True)
+
+    def merge_project_github():
+        """Create a merge request in a GitHub project."""
+        if not github_repos:
+            from github import Auth, Github
+        # get the GitHub object for the given URL:
+        if host_url not in github_repos:
+            github_repos[host_url] = Github(auth=Auth.Token(GITHUB_PRIVATE_TOKEN))
+        gh = github_repos[host_url]
+
+        project = gh.get_repo(repo_path)
+        pr = project.create_pull(
+            base=target_branch,
+            head=source_branch,
+            title=title,
+            body=description
+        )
+        merge_status = pr.merge()
+        if merge_status.merged:
+            # delete the source branch if the merge was successful:
+            project.get_git_ref(f"heads/{source_branch}").delete()
+        else:
+            log.error(f"Merge request {pr.number} could not be merged.")
+
+    remote_repo_parsed = urlparse(remote_repo)
+    host_url = f"{remote_repo_parsed.scheme}://{remote_repo_parsed.netloc}"
+    repo_path = remote_repo_parsed.path.strip("/")
+
+    if "gitlab" in remote_repo_parsed.netloc:
+        merge_project_gitlab()
+    elif "github" in remote_repo_parsed.netloc:
+        merge_project_github()
+    else:
+        log.warning(f"Unknown Git platform: {host_url}")
+
+
+def commit_manifest_changes_to_gear_exchange_repo(
+        repo: Repo,
+        new_branch_name="Update_gear_manifests",
+        commit_message="Update manifests.json"
+) -> bool:
+    """Commit and push the changes to the manifest a new branch.
+
+    This function will commit changes to JSON files in the repo and push them to the remote.
+
+    Args:
+        repo (Repo): the gitpython Repo object for the repository.
+        new_branch_name (str): name of the new branch to be created in the repo.
+        commit_message (str): commit message for the changes.
+
+    Returns:
+        bool: True if there were changes to commit and push, False otherwise.
+    """
+    new_branch = repo.create_head(new_branch_name)
+    new_branch.checkout()
+    repo.git.pull()  # in case there are changes in the remote repo
+    # add the modified manifests to the index (stage them) and commit the changes:
+    repo_changed = False
+    for item in repo.index.diff(None):
+        if item.a_path.endswith(".json"):
+            repo.index.add([item.a_path])
+            repo_changed = True  # bool to track if there are changes to commit
+    if repo_changed:
+        repo.index.commit(commit_message)
+        repo.remotes.origin.push(refspec=f"{new_branch.name}:{new_branch.name}")
+    else:
+        log.warning("No changes to the manifests.\nNot committing or pushing changes.")
+    return repo_changed
+
+
+def save_updated_files_to_csv(gear_list: List[GearDetails], csv_file_path: os.PathLike):
+    """Save the updated gear list to a CSV file."""
+    with open(csv_file_path, 'w') as file:
+        file.write("gear_name,repo_manifest_url\n")
+        for gear in gear_list:
+            file.write(f"{gear.gear_name},{gear.repo_manifest_url}\n")
+    log.info("Updated gear list saved to %s", csv_file_path)
